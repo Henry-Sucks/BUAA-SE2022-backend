0b755bc503133585f981ffa36244ef4733d0b64b
WGX <2690121951@qq.com>
2022-08-02 11:15:53 +0800

    Initial commit

---
+++ src/test/java/com/example/demo/Demo1ApplicationTests.java
@@ -0,0 +1,24 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.annotation.Resource;
+import javax.sql.DataSource;
+import java.sql.SQLException;
+
+@SpringBootTest
+class Demo1ApplicationTests {
+
+    @Autowired
+    @Resource
+    DataSource dataSource;
+
+    @Test
+    void contextLoads() throws SQLException {
+        System.out.println(dataSource.getClass());
+        System.out.println(dataSource.getConnection());
+    }
+
+}
